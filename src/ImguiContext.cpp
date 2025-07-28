f610012a90337341798dd44ae8703dea1959f0f9
刘敏清 ch <2985861993@qq.com>
2025-07-29 05:54:00 +0800

    测试光照，修复对齐为hlsl的对齐，而不是std140，添加文档

---
+++ src/ImguiContext.cpp
@@ -0,0 +1,15 @@
+﻿#include "ImguiContext.h"
+namespace FCT {
+    bool ImguiContext::enableChinese(float size,const char* fontPath)
+    {
+        ImGuiIO& io = ImGui::GetIO();
+        ImFont* font = io.Fonts->AddFontFromFileTTF(fontPath, size, nullptr,
+            io.Fonts->GetGlyphRangesChineseFull());
+        if (font == nullptr) {
+            return false;
+        }
+        io.FontDefault = font;
+        io.Fonts->Build();
+        return true;
+    }
+}
